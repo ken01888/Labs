b3943bd3f452132535b308da70a26f56d937d8d6
ken01888 <77248224+ken01888@users.noreply.github.com>
2021-01-15 22:50:22 -0600

    Add files via upload
    99 lines of code

---
+++ app.js
@@ -0,0 +1,9 @@
+let friends= ["Mike","Mickey","Mendy","Mani","Marvin"];
+
+for (i=0; i<friends.length;i++)
+{
+    for (x=99; x>0 ;x--){
+    console.log(friends[i], "has "+ x + " problems but it ain't coding!");
+}
+};
+
