3d31f8688fdee84aaddc1252f9cfac7cf5e05245
ken01888 <77248224+ken01888@users.noreply.github.com>
2021-01-20 02:34:57 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let friends= ["Mike","Mickey","Mendy","Mani","Marvin"];
 for (i=0; i<friends.length;i++)
 {
     for (x=99; x>1; x--){
-    console.log(friends[i], "has "+ x + " problems but it ain't coding!");
+    console.log(friends[i], `has ${x} problems but it ain't coding!`);
 }
     console.log(friends[i], `has ${1} problem but it ain't coding!`);
 };
